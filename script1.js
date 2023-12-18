a295fd38468cc46ed115d239472d657abe0ca96f
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-12-18 16:38:17 +0700

    update

--- script1.js
+++ script1.js
@@ -1,7 +1,12 @@
 // Cara untuk membuat Object pada javascript
 // 1. Object Literal
 let mahasiswa = {
-    
+    nama: 'Sholahuddin',
+    energi: 10,
+    makan: function(porsi) {
+        this.energi = this.energi + porsi;
+        console.log(`Hallo ${this.nama}, Selamat makan!`);
+    }
 }
 
 
