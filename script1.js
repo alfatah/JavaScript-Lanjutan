42741702a4823a12b9deb0e4e650bd37ff2db5f2
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2023-12-18 16:55:20 +0700

    update

--- script1.js
+++ script1.js
@@ -1,15 +1,64 @@
 // Cara untuk membuat Object pada javascript
+
 // 1. Object Literal
-let mahasiswa = {
-    nama: 'Sholahuddin',
-    energi: 10,
-    makan: function(porsi) {
-        this.energi = this.energi + porsi;
-        console.log(`Hallo ${this.nama}, Selamat makan!`);
+// let mahasiswa1 = {
+//     nama: 'Sholahuddin',
+//     energi: 10,
+//     makan: function(porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Hallo ${this.nama}, Selamat makan!`);
+//     }
+// }
+
+// let mahasiswa2 = {
+//     nama: 'Doddy',
+//     energi: 20,
+//     makan: function(porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Hallo ${this.nama}, Selamat makan!`);
+//     }
+// }
+
+// 2. Function Declaration
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = {};
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
+
+//     mahasiswa.makan = function(porsi) {
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, Selamat makan!`);
+//     }
+
+//     mahasiswa.main = function(jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, Selamat bermain!`)
+//     }
+
+//     return mahasiswa;
+// }
+
+// let sholahuddin = Mahasiswa('Sholahuddin',10);
+// let doddy = Mahasiswa('Doody',20);
+
+// 3. Constructor Function
+// keyword new
+function Mahasiswa(nama, energi) {
+    this.nama = nama;
+   this.energi = energi;
+
+    this.makan = function(porsi) {
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, Selamat makan!`);
+    }
+
+   this.main = function(jam) {
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, Selamat bermain!`)
     }
 }
 
+let sholahuddin = new Mahasiswa('Sholahuddin', 10);
+let doddy = new Mahasiswa('Doddy', 20);
 
-// 2. Function Declaration
-// 3. Constructor Function
 // 4. Object.create
