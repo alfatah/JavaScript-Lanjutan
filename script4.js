19138925eb37dc6e3fb1a63070426c3126ca3a01
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-01-05 15:40:34 +0700

    update

--- script4.js
+++ script4.js
@@ -10,3 +10,17 @@
 // window = global object
 // this = window
 
+// execution phase 
+
+// https://pythontutor.com/javascript.html#mode=edit
+
+var nama = 'Sholahuddin';
+var umur = 33;
+
+console.log(sayHello());
+
+function sayHello() {
+    return `Halo, nama saya ${nama}, saya ${umur} tahun.`;
+}
+
+
