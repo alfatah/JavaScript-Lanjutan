d8a9fa70656b28a8e95cfc4950183ea8a80d49eb
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-01-07 00:30:59 +0700

    update

--- script6.js
+++ script6.js
@@ -1,3 +1,45 @@
-console.log(i);
-var i = 10;
+// let i = 10;
+// console.log(i);
 
+
+// function tes() {
+// for( var i = 0; i < 10; i++ ) {
+//     console.log(i);
+//     }
+// }
+
+// tes();
+// console.log(i);
+
+// (function() {
+    // for( let i = 0 ; i < 10; i++ ) {
+    //     console.log(i);
+    // }
+// }());
+// console.log(i);
+
+// let i = 10;
+// const i = 10;
+// i = 15;
+
+// for( let i = 0; i < 10; i++ )
+// const i = 10;
+// i = 15;
+
+// const mhs = {
+//     nama: "Sholahuddin",
+//     umur: 31
+// }
+
+// mhs = {
+//     jurusan : "Teknik Informatika"
+// }
+
+// console.log(mhs);
+
+const i = [1,2,3]
+
+// i = [1,2,3,4,];
+i.push(4);
+
+console.log(i);
