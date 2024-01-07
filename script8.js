cff26b341950d1b33a20866f6baa5df0845c9491
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-01-07 15:41:45 +0700

    update

---
+++ script8.js
@@ -0,0 +1,51 @@
+// Konsep this pada Arrow Function
+
+// Constructor Function
+// const Mahasiwa = function() {
+//     this.nama = 'Sholahuddin';
+//     this.umur = 33;
+//     console.log(this);
+//     this.sayHello = function() {
+//         console.log(`Halo, nama saya $(nama), dan saya $(umur) tahun.`);
+//     }
+// } 
+
+// const sholahuddin = new Mahasiwa();
+
+// Arrow Function
+// const Mahasiwa = function () {
+//     this.nama = 'Sholahuddin';
+//     this.umur = 33;
+//     this.sayHello = () => {
+//         console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//     }
+// } 
+
+// const sholahuddin = new Mahasiwa();
+
+
+// Object Literal
+// const mhs1 = {
+//     nama: 'Sholahuddin',
+//     umur: '33',
+//     sayHello: () => {
+        // console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`);
+//         console.log(this);
+//     }
+// }
+
+// const Mahasiwa = function() {
+//     this.nama = 'Sholahuddin';
+//     this.umur = 33;
+//     this.sayHello = function() {
+//         console.log(`Halo, nama saya $(nama), dan saya $(umur) tahun.`);
+//     }
+
+//     setInterval(() => {
+//         console.log(this.umur++);
+//     }, 500);
+
+// } 
+
+// const sholahuddin = new Mahasiwa();
+
