eca9ed52afce3dd3b070412909225d1c156b6762
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-01-10 14:14:02 +0700

    update

---
+++ script11.js
@@ -0,0 +1,16 @@
+// ambil semua elemen video
+const videos = document.querySelectorAll()
+
+
+// pilih hanya yang 'JAVASCRIPT LANJUTAN'
+
+// ambil durasi masing2 video
+
+// ubah durasi menjadi int, ubah menit menjadi detik
+
+// jumlahkan semua detik
+
+// ubah formatnya jadi jam menit detik
+
+// simpan di DOM
+
