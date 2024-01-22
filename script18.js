dc6a20a0aaefc92d91a2abb099f8c60d4650fba1
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-01-22 22:55:00 +0700

    update

--- script18.js
+++ script18.js
@@ -2,5 +2,37 @@
 // memecah iterables menjadi single element
 
 
-const mhs = ['Sholahuddin', 'Alfatah', 'Erik'];
-console.log(mhs);
+// const mhs = ['Sholahuddin', 'Alfatah', 'Erik'];
+// console.log(...mhs[0]);
+
+// Menggabungkan 2 array
+// const mhs = ['Sholahuddin', 'Alfatah', 'Erik'];
+// const dosen = ['Ade', 'Hendra', 'Wanda'];
+// const orang = [...mhs, 'Aji', ...dosen];
+// const orang = [...mhs, ...dosen];
+// const orang = mhs.concat(dosen);
+// console.log(orang);
+
+
+// meng-copy array
+// const mhs = ['Sholahuddin', 'Alfatah', 'Erik'];
+// const mhs1 = mhs;
+// const mhs1 = [...mhs];
+// mhs1[0] = 'Fajar';
+// console.log(mhs1);
+
+// const liMhs = document.querySelectorAll('li');
+// console.log(liMhs[0].textContent);
+// const mhs = [];
+// for (let i = 0; i < liMhs.length; i++) {
+//     mhs.push(liMhs[i].textContent);
+// }
+
+// console.log(mhs);
+// const mhs = [...liMhs].map(m => m.textContent);
+// console.log(mhs);
+
+const nama = document.querySelector('.nama');
+const huruf = [...nama.textContent].map
+(h => `<span>${h}</span>`).join('');
+nama.innerHTML = huruf;
