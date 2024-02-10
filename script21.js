bf1749334b0811384c3242ab64ad076fe18e9f22
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-02-10 19:52:52 +0700

    update

--- script21.js
+++ script21.js
@@ -4,10 +4,27 @@
 //     alert(`Halo, ${nama}`);
 // }
 
-function tampilkanPesan(callback) {
-    const nama = prompt('Masukkan Nama : ');
-    callback(nama);
-}
+// function tampilkanPesan(callback) {
+//     const nama = prompt('Masukkan Nama : ');
+//     callback(nama);
+// }
 
-tampilkanPesan(nama => alert(`Halo, ${nama}`));
+// tampilkanPesan(nama => alert(`Halo, ${nama}`));
 
+
+const mhs = [
+    {
+        "nama": "Sandhika Galih",
+        "nrp": "043040023",
+        "email": "sandhikagalih@unpas.ac.id",
+        "jurusan": "Teknik Informatika",
+        "idDosenWali": 1
+    },
+    {
+            "nama": "Sholahuddin Alfatah",
+            "nrp": "0000007",
+            "email": "alfatah1505@gmail.com",
+            "jurusan": "Teknik Informatika",
+            "idDosenWali": 1
+    },
+];
