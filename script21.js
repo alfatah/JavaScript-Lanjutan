1745795311abb2b1c776cdbad51867631da0acb3
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-02-10 19:38:21 +0700

    update

---
+++ script21.js
@@ -0,0 +1,13 @@
+// Callback
+// Synchronous Callback
+// function halo(nama) {
+//     alert(`Halo, ${nama}`);
+// }
+
+function tampilkanPesan(callback) {
+    const nama = prompt('Masukkan Nama : ');
+    callback(nama);
+}
+
+tampilkanPesan(nama => alert(`Halo, ${nama}`));
+
