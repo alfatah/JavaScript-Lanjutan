d9b2566d34f0c66a46873155ef8d849681272a4e
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-03-13 01:47:40 +0700

    update

--- script21.js
+++ script21.js
@@ -52,24 +52,42 @@
 // console.log('selesai');
 
 // Asynchronous Callback
-function getDataMahasiswa(url, success, error) {
-    let xhr = new XMLHttpRequest();
+// function getDataMahasiswa(url, success, error) {
+//     let xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function() {
-        if( xhr.readyState === 4 ) {
-            if(xhr.status === 200 ) {
-                success(xhr.response);
-            } else if(  xhr.status === 404) {
-                error();
-            }
-        }
+//     xhr.onreadystatechange = function() {
+//         if( xhr.readyState === 4 ) {
+//             if(xhr.status === 200 ) {
+//                 success(xhr.response);
+//             } else if(  xhr.status === 404) {
+//                 error();
+//             }
+//         }
+//     }
+//     xhr.open('get', url);
+//     xhr.send();
+// }
+
+
+// console.log('mulai');
+// getDataMahasiswa('data/mahasiswa.json', results => {
+//     const mhs = JSON.parse(results); 
+//     mhs.forEach(m => console.log(m.nama));
+// }, () => {
+// });
+// console.log('selesai');
+
+// JQuery
+console.log('mulai');
+$.ajax({
+    url: 'data/mahasiswa.json',
+    success: (mhs) => {
+        mhs.forEach(m => console.log(m.nama));
+
+    },
+    error: (e) => {
+        console.log(e.responText);
+
     }
-    xhr.open('get', url);
-    xhr.send();
-}
-
-
-getDataMahasiswa('data/mahasiswa.json', (results) => {
-    console.log(results);
-}, () => {
 });
+console.log('selesai');
