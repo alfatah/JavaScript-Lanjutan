7465b478eb1110f1ad3f61199a9ce4799368b814
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-03-13 15:40:49 +0700

    update

---
+++ script22.js
@@ -0,0 +1,28 @@
+// $.ajax({
+//     url: 'http://www.omdbapi.com/?apikey=dca61bcc&s=avengers',
+//     success: movies => console.log(movies) 
+// });
+
+// const xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//     if (xhr.status === 200) {
+//         if (xhr.readyState === 4) {
+//             console.log(JSON.parse(xhr.response));
+//         }
+//     } else {
+//     console.log(xhr.responseText);
+//     }
+// }
+// xhr.open('get', 'http://www.omdbapi.com/?apikey=dca61bcc&s=avengers');
+// xhr.send();
+
+// fetch('http://www.omdbapi.com/?apikey=dca61bcc&s=avengers')
+//     .then(response => response.json())
+//     .then(response => console.log(response));
+
+// Promise
+// Object yang merepresentasikan kerberhasilan / kegagalan sebuah event yang asynchronous di masa yang akan datang
+// janji (terpenuhi / ingkar)
+// states (fulfilled / rejected / pending)
+// callback (resolve / reject / finally)
+// aksi (then)
