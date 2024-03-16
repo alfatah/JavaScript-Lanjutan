09745332bbcf996ff150076e1e01cdc8bd210adb
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-03-17 02:39:27 +0700

    update

--- script23.js
+++ script23.js
@@ -11,12 +11,41 @@ $.ajax({
                             <div class="card-body">
                             <h5 class="card-title">${m.Title}</h5>
                             <h6 class="card-subtitle mb-2 text-body-secondary">${m.Year}</h6>
-                            <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#movieDetailModal">Show Details</a>
+                            <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
                             </div>
                         </div>
                     </div>`
        });
        $('.movie-container').html(cards);
+       
+       // ketika tombol detail di-klik
+       $('.modal-detail-button').on('click', function() {
+        // console.log($(this).data('imdbid'));
+        $.ajax({
+            url: 'http://www.omdbapi.com/?apikey=f6004623&i=' + $(this).data('imdb'),
+            success: m => {
+                const movieDetail = `<div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                        <img src="${m.poster}" alt="img-fluid">
+                                        </div>
+                                        <div class="col md">
+                                        <ul class="list-group">
+                                            <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+                                            <li class="list-group-item"><strong>Director : </strong> ${m.Director}</li>
+                                            <li class="list-group-item"><strong>Actors : </strong> ${m.Actors}</li>
+                                            <li class="list-group-item"><strong>Writer : </strong> ${m.Writer}/li>
+                                            <li class="list-group-item"><strong>Plot : </strong> ${m.Plot}</li>
+                                        </ul>
+                                        </div>
+                                    </div>
+                                    </div>`;
+            }
+        });
+       });
+
+
+
     },
     error: (e) => {
         console.log(e.responseText);
