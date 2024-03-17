770c05c5aea203129a5eb444e096daf41e6a23dd
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-03-18 03:38:49 +0700

    update

--- script23.js
+++ script23.js
@@ -1,70 +1,66 @@
-$('.search-button').on('click', function () {
+$('.search-button').on('click', function (event) {
+    event.preventDefault(); // Menghentikan perilaku default dari tombol pencarian
+
     $.ajax({
         url: 'http://www.omdbapi.com/?apikey=f6004623&s=' + $('.input-keyword').val(),
         success: results => {
-           const movies = results.Search;
-           console.log(movies);
-           let cards = '';
-           movies.forEach(m => {
-            cards += showCards(m);
-           });
-           $('.movie-container').html(cards);
-           
-           // ketika tombol detail di-klik
-           $('.modal-detail-button').on('click', function() {
-            // console.log($(this).data('imdbID'));
-            $.ajax({
-                url: 'http://www.omdbapi.com/?apikey=f6004623&i=' + $(this).data('imdbID'),
-                success: m => {
-                    const movieDetail = showMovieDetail(m);
-                    $('.modal-body').html(movieDetail);
-                },
-                error: (e) => {
-                    console.log(e.responseText);
-                } 
+            const movies = results['Search']; // Menggunakan tanda kurung siku untuk akses properti
+            console.log(movies);
+            let cards = '';
+            movies.forEach(m => {
+                cards += showCards(m);
             });
-           });
-    
-    
+            $('.movie-container').html(cards);
+
+            // Ketika tombol detail di-klik
+            $('.modal-detail-button').on('click', function () {
+                $.ajax({
+                    url: 'http://www.omdbapi.com/?apikey=f6004623&i=' + $(this).data('imdbid'),
+                    success: m => {
+                        console.log(m);
+                        const movieDetail = showMovieDetail(m);
+                        $('.modal-body').html(movieDetail);
+                    },
+                    error: (e) => {
+                        console.log(e.responseText);
+                    }
+                });
+            });
         },
         error: (e) => {
             console.log(e.responseText);
         }
     });
-    
 });
-
-
 
 function showCards(m) {
     return `<div class="col-md-4 my-3">
-                        <div class="card">
-                            <img src="${m.Poster}" class="card-img-top">
-                            <div class="card-body">
-                            <h5 class="card-title">${m.Title}</h5>
-                            <h6 class="card-subtitle mb-2 text-body-secondary">${m.Year}</h6>
-                            <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
-                            </div>
-                        </div>
-                    </div>`;
+        <div class="card">
+            <img src="${m['Poster']}" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${m['Title']}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">${m['Year']}</h6>
+                <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m['imdbID']}">Show Details</a>
+            </div>
+        </div>
+    </div>`;
 }
-
 
 function showMovieDetail(m) {
     return `<div class="container-fluid">
-                                    <div class="row">
-                                        <div class="col-md-3">
-                                        <img src="${m.Poster}" width="190" height="300" alt="img-fluid">
-                                        </div>
-                                        <div class="col md">
-                                        <ul class="list-group">
-                                            <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
-                                            <li class="list-group-item"><strong>Director : </strong> ${m.Director}</li>
-                                            <li class="list-group-item"><strong>Actors : </strong> ${m.Actors}</li>
-                                            <li class="list-group-item"><strong>Writer : </strong> ${m.Writer}</li>
-                                            <li class="list-group-item"><strong>Plot : </strong> ${m.Plot}</li>
-                                        </ul>
-                                        </div>
-                                    </div>
-                                    </div>`;
+        <div class="row">
+            <div class="col-md-3">
+                <img src="${m['Poster']}" width="190" height="300" alt="img-fluid">
+            </div>
+            <div class="col-md">
+                <ul class="list-group">
+                    <li class="list-group-item"><h4>${m['Title']} (${m['Year']})</h4></li>
+                    <li class="list-group-item"><strong>Director : </strong> ${m['Director']}</li>
+                    <li class="list-group-item"><strong>Actors : </strong> ${m['Actors']}</li>
+                    <li class="list-group-item"><strong>Writer : </strong> ${m['Writer']}</li>
+                    <li class="list-group-item"><strong>Plot : </strong> ${m['Plot']}</li>
+                </ul>
+            </div>
+        </div>
+    </div>`;
 }
