d84675617ba4555c7e0a68befc543375b41647b4
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-03-27 09:41:59 +0700

    update

--- script24.js
+++ script24.js
@@ -3,7 +3,7 @@
 const searchButton = document.querySelector('.search-button');
 searchButton.addEventListener('click', function() {
 
-    const inputKeyword = document.querySelector('input-keyword');
+    const inputKeyword = document.querySelector('.input-keyword');
     fetch('http://www.omdbapi.com/?apikey=f6004623&s=' + inputKeyword.value)
     .then(response => response.json())
     .then(response => console.log(response));
