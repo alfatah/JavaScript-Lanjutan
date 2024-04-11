d4f83de784638e650803c21c187bc5685b0eca41
Sholahuddin Alfatah <sholahuddin1999@gmail.com>
2024-04-11 17:17:20 +0700

    update

--- script24.js
+++ script24.js
@@ -1,15 +1,37 @@
-
-// fetch
+// Menggunakan fetch
 const searchButton = document.querySelector('.search-button');
-searchButton.addEventListener('click', function() {
 
+searchButton.addEventListener('click', function() {
     const inputKeyword = document.querySelector('.input-keyword');
+
     fetch('http://www.omdbapi.com/?apikey=f6004623&s=' + inputKeyword.value)
     .then(response => response.json())
     .then(response => {
         const movies = response.Search;
+        let cards = '';
+        movies.forEach(m => cards += showCards(m));
+        const movieContainer = document.querySelector('.movie-container'); 
+        movieContainer.innerHTML = cards;
+
+        // ketika tombol detail di-klik
+        const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+        modalDetailButton.forEach(btn => {
+            btn.addEventListener('click', function() { 
+            const imdbid = this.dataset.imdbid;
+            console.log(imdbid);
+            fetch('http://www.omdbapi.com/?apikey=f6004623&i=' + imdbid)
+                .then(response => response.json())
+                .then(m => {
+                    const movieDetail = showMovieDetail(m);
+                    const modalBody = document.querySelector('.modal-body');
+                    modalBody.innerHTML = movieDetail;
+
+                });
+            });
+        }); 
+        
     });
-    
+
 });
 
 function showCards(m) {
@@ -43,4 +65,3 @@ function showMovieDetail(m) {
         </div>
     </div>`;
 }
-
